f24af83aa8ccf7d885b591224bb0430a81235e07
raccoon0814 <yjh44242168@gmail.com>
2023-04-12 17:18:33 +0900

    bare minimum

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import CountUpDown from './components/countUpdown';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from '@reduxjs/toolkit';
+
+export const increase = () => {
+  return {
+    type: 'INCREASE',
+  };
+};
+
+export const decrease = () => {
+  return {
+    type: 'DECREASE',
+  };
+};
+
+const count = 1;
+
+const counterReducer = (state = count, action) => {
+  switch (action.type) {
+    case 'INCREASE':
+      return state + 1;
+
+    case 'DECREASE':
+      return state - 1;
+
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const store = createStore(counterReducer);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Provider store={store}>
+          <CountUpDown />
+        </Provider>
       </header>
     </div>
   );
---
+++ src/components/countUpdown.js
@@ -0,0 +1,50 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import styled from 'styled-components';
+import { increase, decrease } from '../App';
+import { useDispatch, useSelector } from 'react-redux';
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const UpdownButton = styled.button`
+  width: 30px;
+  height: 30px;
+  background-color: rgb(36.19.15);
+`;
+
+const NumberContainer = styled.div`
+  text-align: center;
+  color: black;
+  margin-bottom: 20px;
+  font-weight: 700;
+`;
+
+function CountUpDown() {
+  const dispatch = useDispatch();
+
+  const plusNum = () => {
+    dispatch(increase());
+  };
+
+  const minusNum = () => {
+    dispatch(decrease());
+  };
+
+  const counter = useSelector((state) => state);
+
+  return (
+    <div>
+      <NumberContainer>{`Counter: ${counter}`}</NumberContainer>
+
+      <ButtonContainer>
+        <UpdownButton onClick={plusNum}> + </UpdownButton>
+        <UpdownButton onClick={minusNum}> - </UpdownButton>
+      </ButtonContainer>
+    </div>
+  );
+}
+
+export default CountUpDown;
